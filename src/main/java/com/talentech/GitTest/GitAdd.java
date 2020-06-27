803e916df57d48091040ba5e345cdb629ddad0a4
Mahbuba Alkhazra Nimme <mahbuba102@gmail.com>
2020-06-27 00:52:48 -0400

    Added new print lines to GitAdd.java

--- src/main/java/com/talentech/GitTest/GitAdd.java
+++ src/main/java/com/talentech/GitTest/GitAdd.java
@@ -3,6 +3,10 @@ package com.talentech.GitTest;
 public class GitAdd {
 	
 	public static void main(String[] args) {
+		
+		
+		System.out.println("A new class");
+		System.out.println("A new class");
 		System.out.println("A new class");
 	}
 
